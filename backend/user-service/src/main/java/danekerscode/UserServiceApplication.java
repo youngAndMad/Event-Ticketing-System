1b68b8f72f9475f5e25b9169841b9a15f028cfec
daneker <kkraken2005@gmail.com>
2023-08-15 16:42:26 +0600

    backend on docker

--- backend/user-service/src/main/java/danekerscode/UserServiceApplication.java
+++ backend/user-service/src/main/java/danekerscode/UserServiceApplication.java
@@ -1,11 +1,7 @@
 package danekerscode;
 
-import danekerscode.utils.Notification;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class UserServiceApplication {
@@ -14,15 +10,4 @@ public class UserServiceApplication {
         SpringApplication.run(UserServiceApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner runner(
-            KafkaTemplate<String, Notification> br
-    ) {
-        return args -> {
-            br.send(
-                    "notification_topic",
-                    new Notification("welcome to event ticket system", "emao")
-            );
-        };
-    }
 }
